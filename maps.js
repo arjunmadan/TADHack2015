4b67f373c269b894a8e342f1958bd0d9ab5b95ef
Arjun Madan <amadan2@ncsu.edu>
2015-06-13 20:25:43 -0400

    Testing Bandwidth

--- maps.js
+++ maps.js
@@ -13,12 +13,12 @@ function directions(origin, destination) {
 	console.log(encodeURI(url + query));
 	request(url + query, function(error, response, body) {
 		console.log(body['routes']);
-  		temp = body['routes'][0]['legs'][0]['steps'];
-  		console.log(temp);
-  		for(i = 0; i < temp.length; i++)
-		{
-			console.log(temp[i]['html_instructions'] + " for " + temp[i]['distance']['text']);
-		}
+  		//temp = body['routes'][0]['legs'][0]['steps'];
+  		//console.log(temp);
+  		//for(i = 0; i < temp.length; i++)
+		//{
+		//	console.log(temp[i]['html_instructions'] + " for " + temp[i]['distance']['text']);
+		//}
   	});
 	
 }
