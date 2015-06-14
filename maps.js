32b688ec42ac84acaaba4d23908cb5266a78c9f2
Arjun Madan <amadan2@ncsu.edu>
2015-06-14 08:24:12 -0400

    Testing Bandwidth

--- maps.js
+++ maps.js
@@ -6,7 +6,7 @@ var client = new catapult.Client("userId", "apiToken", "apiSecret");
 
 catapult.Client.globalOptions.apiToken = "t-pdbvtsbm7crefjscsduudxa";
 catapult.Client.globalOptions.apiSecret = "zld5gfwxkaclimz2fjm2hzptxol5bwf4cdjujty";
-catapult.Client.globalOptions.userId = "u-m53dmzwgoxverwlyhah7wpa";
+catapult.Client.globalOptions.userId = "u-ijxrm4pft4kdpnq4kti4uny";
 
 var API_KEY = 'AIzaSyC2sLZa8Y1O2m4iGX8EP2vDZttXvlsyHwk';
 
@@ -25,21 +25,21 @@ function directions(origin, destination, replyTo, messageFrom) {
   		var str = '';
   		for(i = 0; i < temp.length; i++)
 		{
-			str += temp[i]['html_instructions'] + " for " + temp[i]['distance']['text'];
+			str = temp[i]['html_instructions'] + " for " + temp[i]['distance']['text'];
 			str = str.replace(/<br>/gi, "\n");
 			str = str.replace(/<p.*>/gi, "\n");
 			str = str.replace(/<a.*href="(.*?)".*>(.*?)<\/a>/gi, "\n");
 			str = str.replace(/<(?:.|\s)*?>/g, "");
 			str += '\n';
+			catapult.Message.create({from: messageFrom, to: replyTo, text: str}, function(err, message){
+			if(err){
+    			return console.error(err.message);
+  			}
+  				console.log("Message id is " + message.id);});
 		}
 		console.log(str);
-		for(i = 0; i < str.length; i+=150)
-			catapult.Message.create({from: messageFrom, to: replyTo, text: str.substring(i, i+150)}, function(err, message){
-				if(err){
-    				return console.error(err.message);
-  				}
-  					console.log("Message id is " + message.id);});
-  			});
+		
+  	});
 
 	
 }
