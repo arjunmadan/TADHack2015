f96958be97324151ea747e9da733d568922bb206
Arjun Madan <amadan2@ncsu.edu>
2015-06-14 00:58:59 -0400

    Testing Bandwidth

--- weather.js
+++ weather.js
@@ -21,8 +21,8 @@ function sendGETRequest(query){
 		temp = JSON.parse(body);
 		console.log(temp);
 		str = temp['query']['results']['channel']['item']['description'];
-		str=str.replace(/<br>/gi, "");
-		str=str.replace(/<p.*>/gi, "");
+		str=str.replace(/<br>/gi, "\n");
+		str=str.replace(/<p.*>/gi, "\n");
 		str=str.replace(/<a.*href="(.*?)".*>(.*?)<\/a>/gi, "");
 		str=str.replace(/<(?:.|\s)*?>/g, "");
 		console.log(str);
